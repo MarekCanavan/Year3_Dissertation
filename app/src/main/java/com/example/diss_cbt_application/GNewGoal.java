d74231d9b31675e077f4e871c230ee52f6594939
marek <marek14.canavan@gmail.com>
2020-03-04 10:22:08 +0000

    Goals Page MVP - Date and Time Picker Working

--- app/src/main/java/com/example/diss_cbt_application/GNewGoal.java
+++ app/src/main/java/com/example/diss_cbt_application/GNewGoal.java
@@ -2,15 +2,74 @@ package com.example.diss_cbt_application;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+import java.util.Calendar;
 
 public class GNewGoal extends AppCompatActivity {
+
+    private int mYear, mMonth, mDay, mHour, mMinute;
+    EditText et_goal_date, et_goal_time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gnew_goal);
+
+        et_goal_date = (EditText) findViewById(R.id.et_goal_date);
+        et_goal_time = (EditText) findViewById(R.id.et_goal_time);
+
+    }
+
+    public void chooseDateOnClick(View v){
+
+        // Get Current Date
+        final Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker view, int year,
+                                          int monthOfYear, int dayOfMonth) {
+
+                        et_goal_date.setText(dayOfMonth + "-" + (monthOfYear + 1) + "-" + year);
+
+                    }
+                }, mYear, mMonth, mDay);
+        datePickerDialog.show();
+
+    }
+
+    public void chooseTimeOnClick(View v){
+
+        // Get Current Time
+        final Calendar c = Calendar.getInstance();
+        mHour = c.get(Calendar.HOUR_OF_DAY);
+        mMinute = c.get(Calendar.MINUTE);
+
+        // Launch Time Picker Dialog
+        TimePickerDialog timePickerDialog = new TimePickerDialog(this,
+                new TimePickerDialog.OnTimeSetListener() {
+
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay,
+                                          int minute) {
+
+                        et_goal_time.setText(hourOfDay + ":" + minute);
+                    }
+                }, mHour, mMinute, false);
+        timePickerDialog.show();
+
     }
 
     public void saveGoalOnClick(View v){
